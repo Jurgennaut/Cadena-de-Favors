4236aefb0009a1ebb26884d7a8e8cd0bee70da50
Jordi Oliver <oliver.romero.jordi@alumnat.copernic.cat>
2022-11-16 00:00:03 +0100

    nav_graph

--- app/src/main/java/com/example/cadenadefavors/ProfileFragment.kt
+++ app/src/main/java/com/example/cadenadefavors/ProfileFragment.kt
@@ -1,15 +1,24 @@
 package com.example.cadenadefavors
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cadenadefavors.databinding.FragmentProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+/*private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"*/
+
+private var _binding: FragmentProfileBinding? = null
+private val binding get() = _binding!!
+
+private lateinit var recyclerView: RecyclerView
+private var isLinearLayoutManager = true
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +27,60 @@ private const val ARG_PARAM2 = "param2"
  */
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
+        setup()
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //recyclerView = binding.recyclerView
+        //chooseLayout()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_principal, menu)
+
+/*        val layoutButton = menu.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)*/
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        /*return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }*/
+        return true
+    }
+
+    /*private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,18 +97,17 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+    }*/
 
-    companion object {
-        /**
+    /*companion object {
+        *
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
          * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+         TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ProfileFragment().apply {
@@ -55,5 +116,34 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }*/
+
+    private fun setup() {
+        binding.addOfferBtn.setOnClickListener(){
+            val action = ProfileFragmentDirections.actionProfileFragmentToBlankFragment()
+        }
+    }
+
+    private fun chooseLayout() {
+        /*when (isLinearLayoutManager) {
+            true -> {
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = LetterAdapter()
+            }
+            false -> {
+                recyclerView.layoutManager = GridLayoutManager(context, 4)
+                recyclerView.adapter = LetterAdapter()
+            }
+        }*/
+    }
+
+    private fun setIcon(menuItem: MenuItem?) {
+        /*if (menuItem == null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)*/
     }
 }
