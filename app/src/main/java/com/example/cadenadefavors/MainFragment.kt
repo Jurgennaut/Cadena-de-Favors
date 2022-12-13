796a8a3103bfb14af951fb36f50cd6af3c8d9cf3
Gerardo Vega <vega.mantilla.gerardo.miguel@alumnat.copernic.cat>
2022-12-13 20:52:45 +0100

    bottom test y recyclerview con firebase

--- app/src/main/java/com/example/cadenadefavors/MainFragment.kt
+++ app/src/main/java/com/example/cadenadefavors/MainFragment.kt
@@ -22,6 +22,7 @@ import com.example.cadenadefavors.models.Offer
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 
 /**
@@ -32,7 +33,6 @@ import com.google.firebase.ktx.Firebase
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-
     private lateinit var auth: FirebaseAuth
     val db = Firebase.firestore
 
@@ -61,7 +61,6 @@ class MainFragment : Fragment() {
 
         binding.button6.setOnClickListener{
         Toast.makeText(context, auth.currentUser?.email.toString(),Toast.LENGTH_LONG).show()
-
         }
 
         setupRecyclerView()
@@ -102,14 +101,58 @@ class MainFragment : Fragment() {
         binding.rvOffers.layoutManager = GridLayoutManager(context,2);
 
         //generem el adapter
-        myAdapter.OffersRecyclerAdapter(getOffers(),requireContext())
+        val offers: MutableList<Offer> = arrayListOf()
+        val offersArr = db.collection("favors (cataleg)").get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(TAG, document.data["Description"].toString())
+                    offers.add(Offer(
+                        document.data["Title"].toString(),
+                        document.data["Owner"].toString(),
+                        document.data["Category"].toString(),
+                        document.data["Price"].toString().toInt(),
+                        document.data["Description"].toString(),
+                        document.data["Image"].toString(),
 
-        //assignem el adapter al RV
-        binding.rvOffers.adapter = myAdapter
+                    ))
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }.addOnCompleteListener {
+                offers.add(Offer(
+                    "Entrepans!!",
+                    "restaurant_Amable",
+                    "Menjar",
+                    0,
+                    "Entrepans de tot tipus. Obrir xat per preguntar els preus",
+                    "https://okdiario.com/img/2022/02/08/receta-de-bocata-trufado.jpg"
+                ))
+                myAdapter.OffersRecyclerAdapter(offers,requireContext())
+                binding.rvOffers.adapter = myAdapter
+            }
+
     }
     private fun getOffers() : MutableList<Offer>{
-
         val offers: MutableList<Offer> = arrayListOf()
+        val offersArr = db.collection("favors (cataleg)").get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(TAG, document.data["Description"].toString())
+                    offers.add(Offer(
+                        document.data["Title"].toString(),
+                        document.data["Owner"].toString(),
+                        document.data["Category"].toString(),
+                        document.data["Price"].toString().toInt(),
+                        document.data["Description"].toString(),
+                        document.data["Image"].toString(),
+                    ))
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
+
         offers.add(Offer(
             "Entrepans!!",
             "restaurant_Amable",
@@ -118,62 +161,62 @@ class MainFragment : Fragment() {
             "Entrepans de tot tipus. Obrir xat per preguntar els preus",
             "https://okdiario.com/img/2022/02/08/receta-de-bocata-trufado.jpg"
         ))
-        offers.add(Offer(
-            "PASSEJO GOSSOS",
-            "carles_Cirera",
-            "Animals de companyia",
-            15,
-            "M'ofereixo per passejar gossos. Tinc experiencia passejant diferents races de gos. 15/h",
-            "https://cdn.royalcanin-weshare-online.io/_lbminYBBKJuub5q6J5F/v1/vf-a-spasso-col-cane-una-salute-un-benessere-442-800?h=675&w=1200&la=es-ES&fm=jpg"
-        ))
-        offers.add(Offer(
-            "CORTO EL CESPED",
-            "albert_mateos",
-            "Serveis domestics",
-            5,
-            "Corto el cesped de tu patio, 5 favos",
-            "https://zulueta.com/wp-content/uploads/2017/03/cortar_cesped_fb.jpg"
-        ))
-        offers.add(Offer(
-            "Te hago un bocata",
-            "jordi_oliver",
-            "Menjar",
-            5,
-            "Te hago un bocata de lo que tenga por casa",
-            "https://canalcocina.es/medias/_cache/zoom-72c7e28383e6bdc2f4399a9a61bf3cb7-920-518.jpg"
-        ))
-        offers.add(Offer(
-            "Entrepans!!",
-            "restaurant_Amable",
-            "Menjar",
-            0,
-            "Entrepans de tot tipus. Obrir xat per preguntar els preus",
-            "https://okdiario.com/img/2022/02/08/receta-de-bocata-trufado.jpg"
-        ))
-        offers.add(Offer(
-            "PASSEJO GOSSOS",
-            "carles_Cirera",
-            "Animals de companyia",
-            15,
-            "M'ofereixo per passejar gossos. Tinc experiencia passejant diferents races de gos. 15/h",
-            "https://cdn.royalcanin-weshare-online.io/_lbminYBBKJuub5q6J5F/v1/vf-a-spasso-col-cane-una-salute-un-benessere-442-800?h=675&w=1200&la=es-ES&fm=jpg"
-        ))
-        offers.add(Offer(
-            "CORTO EL CESPED",
-            "albert_mateos",
-            "Serveis domestics",
-            5,
-            "Corto el cesped de tu patio, 5 favos",
-            "https://zulueta.com/wp-content/uploads/2017/03/cortar_cesped_fb.jpg"
-        ))
-        offers.add(Offer(
-            "Te hago un bocata",
-            "jordi_oliver",
-            "Menjar",
-            5,
-            "Te hago un bocata de lo que tenga por casa",
-            "https://canalcocina.es/medias/_cache/zoom-72c7e28383e6bdc2f4399a9a61bf3cb7-920-518.jpg"
-        ))
+//        offers.add(Offer(
+//            "PASSEJO GOSSOS",
+//            "carles_Cirera",
+//            "Animals de companyia",
+//            15,
+//            "M'ofereixo per passejar gossos. Tinc experiencia passejant diferents races de gos. 15/h",
+//            "https://cdn.royalcanin-weshare-online.io/_lbminYBBKJuub5q6J5F/v1/vf-a-spasso-col-cane-una-salute-un-benessere-442-800?h=675&w=1200&la=es-ES&fm=jpg"
+//        ))
+//        offers.add(Offer(
+//            "CORTO EL CESPED",
+//            "albert_mateos",
+//            "Serveis domestics",
+//            5,
+//            "Corto el cesped de tu patio, 5 favos",
+//            "https://zulueta.com/wp-content/uploads/2017/03/cortar_cesped_fb.jpg"
+//        ))
+//        offers.add(Offer(
+//            "Te hago un bocata",
+//            "jordi_oliver",
+//            "Menjar",
+//            5,
+//            "Te hago un bocata de lo que tenga por casa",
+//            "https://canalcocina.es/medias/_cache/zoom-72c7e28383e6bdc2f4399a9a61bf3cb7-920-518.jpg"
+//        ))
+//        offers.add(Offer(
+//            "Entrepans!!",
+//            "restaurant_Amable",
+//            "Menjar",
+//            0,
+//            "Entrepans de tot tipus. Obrir xat per preguntar els preus",
+//            "https://okdiario.com/img/2022/02/08/receta-de-bocata-trufado.jpg"
+//        ))
+//        offers.add(Offer(
+//            "PASSEJO GOSSOS",
+//            "carles_Cirera",
+//            "Animals de companyia",
+//            15,
+//            "M'ofereixo per passejar gossos. Tinc experiencia passejant diferents races de gos. 15/h",
+//            "https://cdn.royalcanin-weshare-online.io/_lbminYBBKJuub5q6J5F/v1/vf-a-spasso-col-cane-una-salute-un-benessere-442-800?h=675&w=1200&la=es-ES&fm=jpg"
+//        ))
+//        offers.add(Offer(
+//            "CORTO EL CESPED",
+//            "albert_mateos",
+//            "Serveis domestics",
+//            5,
+//            "Corto el cesped de tu patio, 5 favos",
+//            "https://zulueta.com/wp-content/uploads/2017/03/cortar_cesped_fb.jpg"
+//        ))
+//        offers.add(Offer(
+//            "Te hago un bocata",
+//            "jordi_oliver",
+//            "Menjar",
+//            5,
+//            "Te hago un bocata de lo que tenga por casa",
+//            "https://canalcocina.es/medias/_cache/zoom-72c7e28383e6bdc2f4399a9a61bf3cb7-920-518.jpg"
+//        ))
 
         return offers
 
